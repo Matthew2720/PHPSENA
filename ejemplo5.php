e76fe2ac00e5c0a6caabed7032a7bc1167e55cf4
Mateo Hernandez <mateo.hernandezr@udea.edu.co>
2022-04-25 07:38:29 -0500

    Carpeta PHP en Xampp

---
+++ ejemplo5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Variables
+        $A=2;$B=6;$x=2;$y=3;
+        $Formula=($A*$x) + ($B*$y);
+
+        echo "El resultado de la formula es : <b>$Formula</b> <br>";
+    ?>
+</body>
+</html>
